ffdd92d868ea6c9eb9fb0d8f9e0d18bfddf96eb4
FantasyVR <y2505418927@gmail.com>
2022-12-01 13:13:45 +0800

    day01

---
+++ day01/day01.py
@@ -0,0 +1,16 @@
+import numpy as np
+with open("input") as f:
+    lines = [line for line in f.readlines()]
+    elf = []
+    sum = 0
+    for i, line in enumerate(lines):
+        if line.strip() == "":
+            elf.append(sum)
+            sum = 0
+            continue
+        energy = int(line)
+        sum += energy
+    elf_np = np.asarray(elf)
+    elf_sort = np.sort(elf_np)
+    print(f"1st problem: {np.max(elf_np)}")
+    print(f"second problem: {elf_sort[-1]+elf_sort[-2]+elf_sort[-3]}")
